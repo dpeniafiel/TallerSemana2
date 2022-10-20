423c1a3358f590ba62eeb4b9eb6e6101e1818478
Diego Peñafiel <dpeniafiel@latablitadeltartaro.com>
2022-10-19 19:13:38 -0500

    Agregar archivos de proyecto.

---
+++ TallerSemana2/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerSemana2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "dpeniafiel";
+            string clave = "123";
+
+            string tUsuario = txtUsuario.Text;
+            string tContrasenia = txtContrasenia.Text;
+
+            if (usuario == tUsuario & clave == tContrasenia)
+            {
+                DisplayAlert("Info", "Usuario correcto", "ok");
+                Navigation.PushAsync(new Registro());
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario  incorrecto", "cerrar");
+
+            }
+        }
+    }
+}
---
+++ TallerSemana2/Registro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerSemana2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInformacion_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double nota = Convert.ToDouble(txtNota.Text);
+
+            if (nota >= 7)
+            {
+
+                DisplayAlert("Mensaje de alerta", "Tu nomre es: " + nombre + ", apellido es: " + apellido, "aprobaste con nota: " + nota);
+            }
+            else
+            {
+                DisplayAlert("Mensaje de alerta", "Tu nomre es: " + nombre + ", apellido es: " + apellido, "reprobaste con nota: " + nota);
+
+            }
+        }
+    }
+}
